a43b5ad32aaea1e9be17b95080395ae138e32ed8
Katherine Rasmussen <krasmussen@lbl.gov>
2025-08-13 10:46:20 -0700

    Add changes to bidirectional file and add cray config file

--- performance/microbenchmarks/src/bidirectional.f90
+++ performance/microbenchmarks/src/bidirectional.f90
@@ -20,7 +20,7 @@
 !---------------------------------------------------------------------
 
 program bidirectional
-
+  use iso_fortran_env, only: output_unit
   implicit none
 
   integer,parameter :: nt=1024*1024
@@ -52,6 +52,7 @@ program bidirectional
 
   if (me == 1) then
      write(* ,'(A1,A10,A21,A20)') "#","[Bytes]","[Microsec]","[KB/sec]"
+     flush(output_unit)
   endif
 
   i=1
@@ -61,7 +62,8 @@ program bidirectional
   do while (msg_size < nt)
 
     if (me == 1) then
-        write(*,'(I2,A1)',advance='no') i,";"
+       write(*,'(I2,A1)',advance='no') i,";"
+       flush(output_unit)
     end if
 
     sync all
@@ -89,7 +91,7 @@ program bidirectional
         r_iterations=iterations
 
         write(*,'(I10,A1,E20.8,A1,E20.8)') 4*msg_size,";",rtc*1000000.0/r_iterations,";",4.0*r_msgsize*r_iterations/rtc/1024.0
-
+        flush(output_unit)
         i=i+1
 
      endif
@@ -99,5 +101,7 @@ program bidirectional
   enddo
 
   deallocate (msg1, msg2)
+
+  stop
 
 end program bidirectional
